651f3ec954c534a2e9cc075217fc47c0e6deca2c
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2023-07-08 19:40:12 -0500

    08/07/2023

--- DIPLOMADO/Ejercicios/ejercicio19.py
+++ DIPLOMADO/Ejercicios/ejercicio19.py
@@ -1,1 +1,11 @@
-num = int(input("Ingrese un número: "))
+numero = int(input("Ingrese un número: "))
+
+suma_divisores = 0
+for i in range(1, numero):
+    if numero % i == 0:
+        suma_divisores += i
+
+if suma_divisores == numero:
+    print("Es un número perfecto")
+else:
+    print("No es un número perfecto")
