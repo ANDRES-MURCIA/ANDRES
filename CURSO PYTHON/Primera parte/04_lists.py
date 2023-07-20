9d5f02917cc7c60c5bf0e4818366c213fa497549
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2023-07-20 11:26:44 -0500

    20/07/2023

--- CURSO PYTHON/Primera parte/04_lists.py
+++ CURSO PYTHON/Primera parte/04_lists.py
@@ -12,7 +12,7 @@ my_list = [35, 24, 62, 52, 30, 30, 17]
 print(my_list)
 print(len(my_list))
 
-my_other_list = [35, 1.77, "Brais", "Moure"]
+my_other_list = [19, 1.77, "Andrés", "Murcia"]
 
 print(type(my_list))
 print(type(my_other_list))
@@ -27,7 +27,7 @@ print(my_list.count(30))
 # print(my_other_list[4]) IndexError
 # print(my_other_list[-5]) IndexError
 
-print(my_other_list.index("Brais"))
+print(my_other_list.index("Andrés"))
 
 age, height, name, surname = my_other_list
 print(name)
@@ -42,7 +42,7 @@ print(my_list + my_other_list)
 
 # Creación, inserción, actualización y eliminación
 
-my_other_list.append("MoureDev")
+my_other_list.append("Torres")
 print(my_other_list)
 
 my_other_list.insert(1, "Rojo")
--- CURSO PYTHON/Primera parte/05_tuples.py
+++ CURSO PYTHON/Primera parte/05_tuples.py
@@ -5,7 +5,7 @@
 my_tuple = tuple()
 my_other_tuple = ()
 
-my_tuple = (35, 1.77, "Brais", "Moure", "Brais")
+my_tuple = (35, 1.77, "Andrés", "Murcia", "Torres")
 my_other_tuple = (35, 60, 30)
 
 print(my_tuple)
@@ -18,9 +18,9 @@ print(my_tuple[-1])
 # print(my_tuple[4]) IndexError
 # print(my_tuple[-6]) IndexError
 
-print(my_tuple.count("Brais"))
-print(my_tuple.index("Moure"))
-print(my_tuple.index("Brais"))
+print(my_tuple.count("Andrés"))
+print(my_tuple.index("Murcia"))
+print(my_tuple.index("Andrés"))
 
 # my_tuple[1] = 1.80 'tuple' object does not support item assignment
 
@@ -38,7 +38,7 @@ print(my_sum_tuple[3:6])
 my_tuple = list(my_tuple)
 print(type(my_tuple))
 
-my_tuple[4] = "MoureDev"
+my_tuple[4] = "Torres"
 my_tuple.insert(1, "Azul")
 my_tuple = tuple(my_tuple)
 print(my_tuple)
