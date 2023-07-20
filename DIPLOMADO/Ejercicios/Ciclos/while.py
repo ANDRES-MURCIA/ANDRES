d6d7b664046d2a0216c40b64d7219f4933d0db27
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2023-07-20 11:50:37 -0500

    Diplomado

--- DIPLOMADO/Ejercicios/Ciclos/while.py
+++ DIPLOMADO/Ejercicios/Ciclos/while.py
@@ -4,10 +4,10 @@ count = 2
 while count <= 100:
     print(count)
     count += 2
-
 # --------------------------------------------
 num = int(input("Ingrese el número: "))
 var = 1
+
 while var <= 10:
     print(num, "x", var, "=", var * num)
     var += 1
