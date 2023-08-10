fc71bf5a866a0b0b51b2757da524de9ff88a3d42
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2023-08-10 18:59:03 -0500

    Proyecto

--- DIPLOMADO/Proyecto_final/proyecto.py
+++ DIPLOMADO/Proyecto_final/proyecto.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox  
 from PIL import Image, ImageTk
-
+ 
 root = tk.Tk()  # Crear la ventana principal
 root.title("Juego de Preguntas")
 root.iconbitmap("icono.ico")
