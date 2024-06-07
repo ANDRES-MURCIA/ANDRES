35f2c78fd61cf13765388bb12374c5709bddaa76
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2024-06-06 19:17:35 -0500

    Java

--- Java projects/src/Tabla.java
+++ Java projects/src/Tabla.java
@@ -5,7 +5,7 @@ public class Tabla {
     public static void main (String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             int i, numero, resultado;
-            System.out.print("Ingrese un n\243mero: ");
+            System.out.print("Ingrese un numero: ");
             numero = in.nextInt();
             System.out.println();
             for (i=1; i<=10; i++) {
