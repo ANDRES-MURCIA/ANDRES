3702111fef2c1fd9fecade6347a6ec72d1b431f3
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2024-09-30 17:32:47 -0500

    Udemy

--- CURSOS UDEMY/Python/Manejo de cadenas/practica.py
+++ CURSOS UDEMY/Python/Manejo de cadenas/practica.py
@@ -2,13 +2,11 @@ print("Generador de email")
 nombre_usuario = "Ubaldo Acosta Soto"
 nombre_usuario = nombre_usuario.lower().strip()
 nombre_usuario = nombre_usuario.replace(" ", ".")
-print(nombre_usuario)
 
 nombre_empresa = "Global Mentoring"
 extension = ".com.mx"
 nombre_empresa = nombre_empresa.lower().strip()
-nombre_empresa = nombre_empresa.replace(" ", "")
-print(nombre_empresa)
+nombre_empresa = f"@{nombre_empresa.replace(" ", "")}"
 
-resultado = f"El correo final es: @{nombre_usuario}{nombre_empresa}{extension}"
+resultado = f"El correo final es: {nombre_usuario}{nombre_empresa}{extension}"
 print(resultado)
