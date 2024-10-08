3765a7ccd3b72807154f5e4d689fb950b41e512a
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2024-10-07 20:57:01 -0500

    Udemy

--- CURSOS UDEMY/Python/04_Operadores/05-02-00-OperadoresAritmeticos-UP.py
+++ CURSOS UDEMY/Python/04_Operadores/05-02-00-OperadoresAritmeticos-UP.py
@@ -13,19 +13,19 @@ print(f"Resta: {resta}")
 
 # Multiplicación *
 multiplicacion = a * b
-print(f"Multiplicación: {multiplicacion}")
+print(f"Multiplicacion: {multiplicacion}")
 
 # División / (retorna un tipo float)
 division = a / b
-print(f"División: {division:.2f}")
+print(f"Division: {division:.2f}")
 
 # División entera (//)
 division_entera = a // b
-print(f"División entera: {division_entera}")
+print(f"Division entera: {division_entera}")
 
 # Modulo (%) residuo de la división
 modulo = a % b
-print(f"Residuo de la división: {modulo}")
+print(f"Residuo de la division: {modulo}")
 
 # Exponente (potencia) **
 exponente = a**b  # 10 elevado a la 3 = 10*10*10=1000
