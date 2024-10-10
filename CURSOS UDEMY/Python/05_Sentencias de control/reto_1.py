3fe31bc6178278d2031dc13bdba73ef0c8230b1a
@ANDRES-MURCIA <andresmurcia0904@gmail.com>
2024-10-09 20:30:23 -0500

    Udemy

--- CURSOS UDEMY/Python/05_Sentencias de control/reto_1.py
+++ CURSOS UDEMY/Python/05_Sentencias de control/reto_1.py
@@ -9,20 +9,20 @@ valor_final_descuento_alto = valor_comprado - descuento_alto
 valor_final_descuento_medio = valor_comprado - descuento_medio
 
 if valor_comprado >= compra_requerida and miembro == "si":
-    print(f"Valor de la compra original: {valor_comprado}")
+    print(f"\nValor de la compra original: {valor_comprado}")
     print(
-        f"""Cumple con los requisitos para el descuento del 10%.
+        f"""\nCumple con los requisitos para el descuento del 10%.
         \nSaldo a pagar {valor_final_descuento_alto}"""
     )
-    print(f"El descuento es: {descuento_alto}")
+    print(f"\nEl descuento es: {descuento_alto}")
 
 elif valor_comprado < compra_requerida and miembro == "si":
-    print(f"Valor de la compra original: {valor_comprado}")
+    print(f"\nValor de la compra original: {valor_comprado}")
     print(
-        f"""Cumple con los requisitos para el descuento del 5%.
+        f"""\nCumple con los requisitos para el descuento del 5%.
         \nSaldo a pagar {valor_final_descuento_medio}"""
     )
-    print(f"El descuento es: {descuento_medio}")
+    print(f"\nEl descuento es: {descuento_medio}")
 
 else:
     print(f"No tienes descuentos, el valor a pagar es: {valor_comprado}")
